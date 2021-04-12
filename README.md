92abe8083b49b327a5180be9b1f04b8bb69f54a2
yimt <2579881587@qq.com>
2021-04-12 23:10:15 +0800

    Add backups.sh

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 > linux工具脚本
 
+- [ulimit config](./limits_config.sh)
 
+- [backups direction](./backups.sh)
 
---
+++ backups.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Note
+# 1.set timezone
+# 2.set backups direction
+
+# 1.set timezone
+# see timezone
+# ls /usr/share/zoneinfo/Asia/
+export TZ=Asia/Shanghai
+time=$(date +"%Y-%m-%d")
+
+# 2.set backups direction
+# direction
+direction_name='you direction name'
+
+tar -zxvf "${direction_name}_${time}.tar.gz" $direction_name
