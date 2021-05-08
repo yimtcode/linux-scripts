c12b83684a5d82bcd413941644a3aad684628a49
yimtcode <2579881587@qq.com>
2021-05-08 13:56:04 +0800

    Add installation script for nginx

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 > Linux tool script
 
+## Config
+
 - [ulimit config](./limits_config.sh)
 
+## Backup
+
 - [backups direction](./backups.sh)
+
+## Soft
+
+- Nginx
+  - [ReadHat|CentOS](./nginx_install_redhat-cenos.sh)
 
---
+++ nginx_install_redhat-cenos.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+sudo yum install -y yum-utils
+
+rm -rf /etc/yum.repos.d/nginx.repo
+
+touch /etc/yum.repos.d/nginx.repo
+
+echo "[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true" >> /etc/yum.repos.d/nginx.repo
+
+sudo yum install -y nginx
